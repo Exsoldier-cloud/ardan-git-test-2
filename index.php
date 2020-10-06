1d2ee438d4fbb8682f1138ceb7663f2d63c6e0a7
Ramdan Dwiansyah Yusuf <ramdandwiansyahyusuf@ft-umt.ac.id>
2020-10-07 02:13:14 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,10 @@
 <body>
     <h1>Halo Dunia!</h1>
     <h2>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis, amet incidunt ipsa voluptas assumenda similique unde eius iste odit commodi, laboriosam labore dolores. Corporis ullam expedita obcaecati pariatur nemo eaque.</h2>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+        <li>tiga</li>
+    </ul>
 </body>
 </html>
